99c216db6357681d5de62edc83643ac7be064604
michele <michele@courseratest.it>
2018-04-05 17:12:07 +0200

    solution2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,59 @@
 (function(){
   'use strict';
-  angular.module('AppModul2Solution'[])
+  angular.module('AppModul2Solution',[])
   .controller('ToBuyController',ToBuyController)
-  .controller('AlreadyBoughtController',AlreadyBoughtController);
-  ToBuyController.$inject = ['$scope'];
-  AlreadyBoughtController.$inject = ['$scope'];
-  function ToBuyController($scope){
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .service('ShareDataService', ShareDataService);
 
+  function ShareDataService(){
+    var service = this;
+    var items = [];
+
+    service.getItems = function (){
+      return items;
+    };
+    service.addItem = function (itemName,quantity){
+      var item = {
+        name: itemName,
+        quantity : quantity
+      };
+    items.push(item);
+    console.log(" Item pushed into ShareDataService Items: ", " Done");
+
+    };
   }
-  function AlreadyBoughtController($scope){
+
+  ToBuyController.$inject = ['ShareDataService'];
+  function ToBuyController(ShareDataService){
+    var vm = this;
+    vm.shoppingList =[
+      {
+        name: 'Cookies',
+        quantity: 1
+      },
+      {
+        name: 'Salad',
+        quantity: 2
+      },
+      {
+        name: 'Muffin',
+        quantity: 5
+      }
+    ];
+    vm.sendList = function (s1, s2){
+      ShareDataService.addItem(s1,s2);
+      console.log(" Item pushed into ShareDataService listItems_: ", " Done");
+    };
+  };
+
+  AlreadyBoughtController.$inject = ['ShareDataService'];
+  function AlreadyBoughtController(ShareDataService){
+    var vm = this;
+    vm.ListSelected =[];
+    vm.getShoppingList = function (){
+      return ShareDataService.getItems();
+    };
+
 
   }
 })();
